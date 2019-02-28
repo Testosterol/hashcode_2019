00528286351b2924fcecf83cd41ae2e512230a9b
peter <pb@calldorado.com>
2019-02-28 19:15:27 +0100

    orientation added to const

--- src/Image.java
+++ src/Image.java
@@ -1,21 +1,24 @@
 public class Image {
-    private enum ORIENTATION {
+    public enum ORIENTATION {
         HORIZONTAL,
         VERTICAL
     }
 
+    private ORIENTATION orientation;
     private TagList tagList;
     private int numberOfTags;
     private int photoId;
 
-    public Image(TagList tagList, int numberOfTags, int photoId) {
+    public Image(ORIENTATION orientation, TagList tagList, int numberOfTags, int photoId) {
         TagList tagsToLower = new TagList();
-        for (Tag tag : tagList){
+        for (Tag tag : tagList) {
             tagsToLower.add(new Tag(tag.getTag().toLowerCase()));
         }
 
+        this.orientation = orientation;
         this.tagList = tagsToLower;
         this.numberOfTags = numberOfTags;
         this.photoId = photoId;
     }
+
 }

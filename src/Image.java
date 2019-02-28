d22e3aed9e2982f5535a369d6d38c8329de08898
peter <pb@calldorado.com>
2019-02-28 19:13:07 +0100

    Tag list input validation

--- src/Image.java
+++ src/Image.java
@@ -1,2 +1,21 @@
 public class Image {
+    private enum ORIENTATION {
+        HORIZONTAL,
+        VERTICAL
+    }
+
+    private TagList tagList;
+    private int numberOfTags;
+    private int photoId;
+
+    public Image(TagList tagList, int numberOfTags, int photoId) {
+        TagList tagsToLower = new TagList();
+        for (Tag tag : tagList){
+            tagsToLower.add(new Tag(tag.getTag().toLowerCase()));
+        }
+
+        this.tagList = tagsToLower;
+        this.numberOfTags = numberOfTags;
+        this.photoId = photoId;
+    }
 }
